3f168555bec13d04919a22643c043f2017125968
zotov88 <zotov_l88@mail.ru>
2023-08-08 20:32:51 +0300

    day05 ex00

---
+++ Java_Bootcamp.Day05-1/src/ex00/Chat/src/main/java/edu/school21/chat/models/Message.java
@@ -0,0 +1,83 @@
+package edu.school21.chat.models;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Message {
+
+    private final Long id;
+    private User author;
+    private Chatroom room;
+    private String text;
+    private LocalDate date;
+
+    public Message(Long id, User author, Chatroom room, String text, LocalDate date) {
+        this.id = id;
+        this.author = author;
+        this.room = room;
+        this.text = text;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Chatroom getRoom() {
+        return room;
+    }
+
+    public void setRoom(Chatroom room) {
+        this.room = room;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id) && Objects.equals(author, message.author)
+                && Objects.equals(room, message.room) && Objects.equals(text, message.text)
+                && Objects.equals(date, message.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, room, text, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", author=" + author +
+                ", room=" + room +
+                ", text='" + text + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
